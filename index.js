0e942606abbab9e3ed298732308e4cb78dd250e4
Camille Briand <camille.briand@efrei.net>
2020-04-28 16:31:02 +0200

    First working prototype

--- index.js
+++ index.js
@@ -1,37 +1,70 @@
 const hostile = require('hostile');
 const redbird = require('redbird');
 const inquirer = require('inquirer');
-const colors = require('colors');
+require('colors');
 
 const { version } = require('./package.json');
 
 console.log(`lurl v${version}`.green);
+
+function cleanHostsFile (domain, callback) {
+	return () => {
+		hostile.remove('127.0.0.1', domain, (err) => {
+			if (err) console.error(err);
+			else callback();
+		});
+	}
+}
 
 const inquirerQuestions = {
 	rproxy: [
 		{
 			type: 'input',
 			name: 'domainName',
-			message: '🔗 Domain name to use: ',
+			message: '🔗 Domain name to use:',
 			validate: input => input && input.length >= 1 || 'Cannot be empty',
 		},
 		{
 			type: 'input',
 			name: 'port',
-			message: '🔌 Port to bind: ',
+			message: '🔌 Port to bind:',
 			validate: input => {
 				const p = parseInt(input);
 				if (isNaN(input) || p == undefined) return 'Must be an integer';
 
-				return ![22, 80, 443].includes(p) || 'Cannot be SSH, HTTP or HTTPS port';
+				return p > 1000 || 'Port must be in [1000 - 65536] range';
 			},
 		}
 	]
 }
 
 inquirer.prompt(inquirerQuestions.rproxy).then(answers => {
-	const { domainName } = answers;
+	const domainName = answers.domainName.toLowerCase();
 	const port = parseInt(answers.port);
 
 	console.log(`Starting to bind ${domainName.yellow} to ${'localhost:'.yellow}${(port+'').yellow}`);
+
+	hostile.set('127.0.0.1', domainName, (err) => {
+		if (err) {
+			console.error(err);
+			process.exit();
+		}
+
+		const cleanExit = cleanHostsFile(domainName, () => process.exit());
+		process.on('SIGINT', cleanExit);
+		process.on('exit', cleanExit);
+
+		const proxy = redbird({ 
+			port: 80,
+			bunyan: false
+		});
+
+		console.log(`Registered ${domainName.yellow} in hosts file`);
+
+		proxy.register(`${domainName}`, `http://localhost:${port}`);
+
+		console.log(`Register ${domainName.yellow} virtual host to ${('localhost:'+port).yellow}`);
+
+		console.log(`Listening for requests`.america);
+	});
 });
